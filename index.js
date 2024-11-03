de33991096894b49d3c5b8a20d3f1ae57bc74106
Mohmmed-Saffan <mdshafan51@gmail.com>
2024-11-03 23:57:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import multer from "multer";
+import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
+import path from "path";
+
+const app = express();
+
+//multer middleware
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname));
+  },
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); //watch this
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Saffu" });
+});
+
+app.listen(8000, () => {
+  console.log("Listening from server..........");
+});
